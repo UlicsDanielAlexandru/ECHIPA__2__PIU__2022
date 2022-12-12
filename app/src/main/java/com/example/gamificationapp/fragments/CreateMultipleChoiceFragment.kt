1edf72fb2b8dccf818568659f0c2f8721b25a45b
Andrei Suta <andrei.suta@rf-meters.com>
2022-12-13 00:24:11 +0200

    Added some screens in android

---
+++ app/src/main/java/com/example/gamificationapp/fragments/CreateMultipleChoiceFragment.kt
@@ -0,0 +1,9 @@
+package com.example.gamificationapp.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.gamificationapp.R
+
+
+class CreateMultipleChoiceFragment : Fragment(R.layout.fragment_create_multiple_choice) {
+
+}
---
+++ app/src/main/java/com/example/gamificationapp/fragments/DropDownsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.gamificationapp.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.gamificationapp.R
+
+class DropDownsFragment : Fragment(R.layout.fragment_drop_downs) {
+
+}
